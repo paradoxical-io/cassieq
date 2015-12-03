09fddb41a1e8fbde6c1ab665c1328d9270e377c8
Anton Kropp <akropp@godaddy.com>
2015-12-03 10:20:05 -0800

    Sha short changes

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 set -x
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  mvn clean deploy --settings settings.xml -DskipTests
+    mvn clean deploy --settings settings.xml -DskipTests
 
-  docker tag -f paradoxical/cassieq:${TRAVIS_COMMIT}_dev paradoxical/cassieq
+    SHA_SHORT=`git rev-parse --short HEAD`
 
-  docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-  docker push paradoxical/cassieq:${TRAVIS_COMMIT}_dev
-  docker push paradoxical/cassieq
+    docker tag -f paradoxical/cassieq:${SHA_SHORT}_dev paradoxical/cassieq
+
+    docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+    docker push paradoxical/cassieq:${SHA_SHORT}_dev
+    docker push paradoxical/cassieq
 fi
