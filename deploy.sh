1c569e70906f3a5b4210ea89267d2e61898ce101
Anton Kropp <akropp@godaddy.com>
2015-12-02 18:30:18 -0800

    Adding docker deploys

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,10 @@ set -x
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   mvn clean deploy --settings settings.xml -DskipTests
+
+  docker tag -f paradoxical/cassieq:${TRAVIS_COMMIT}_dev paradoxical/cassieq
+
+  docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+  docker push paradoxical/cassieq:${TRAVIS_COMMIT}_dev
+  docker push paradoxical/cassieq
 fi
