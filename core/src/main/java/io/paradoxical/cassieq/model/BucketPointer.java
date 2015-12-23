1198d87ff2f94e3ddce7316a2cc85cd4c6e3216f
Anton Kropp <akropp@godaddy.com>
2015-12-22 19:15:03 -0800

    Adding generic bucket pointer

--- core/src/main/java/io/paradoxical/cassieq/model/BucketPointer.java
+++ core/src/main/java/io/paradoxical/cassieq/model/BucketPointer.java
@@ -2,7 +2,7 @@ package io.paradoxical.cassieq.model;
 
 public interface BucketPointer extends Pointer {
     default BucketPointer next(){
-        return ReaderBucketPointer.valueOf(get() + 1);
+        return GenericBucketPointer.valueOf(get() + 1);
     }
 
     default MonotonicIndex startOf(BucketSize bucketsize){
