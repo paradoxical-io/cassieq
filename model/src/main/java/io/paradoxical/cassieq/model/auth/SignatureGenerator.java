22d4329e5bfa57c262289100dfb81a297a2d82a2
Jake Swenson <jswenson@godaddy.com>
2016-02-13 10:56:02 -0800

    use guava encoding

--- model/src/main/java/io/paradoxical/cassieq/model/auth/SignatureGenerator.java
+++ model/src/main/java/io/paradoxical/cassieq/model/auth/SignatureGenerator.java
@@ -1,6 +1,7 @@
 package io.paradoxical.cassieq.model.auth;
 
 import com.godaddy.logging.Logger;
+import com.google.common.io.BaseEncoding;
 
 import javax.crypto.Mac;
 import java.util.Base64;
@@ -8,22 +9,22 @@ import java.util.Base64;
 import static com.godaddy.logging.LoggerFactory.getLogger;
 
 /**
- * Signs paramters
+ * Computes a signature of a string to sign.
  */
 public abstract class SignatureGenerator {
+
+    private static final BaseEncoding signatureEncoding = BaseEncoding.base64Url().omitPadding();
 
     public String computeSignature(final Mac hmac) {
         final Logger logger = getLogger(SignatureGenerator.class);
 
         final String stringToSign = getStringToSign();
 
-        final Base64.Encoder signatureEncoder = Base64.getUrlEncoder().withoutPadding();
-
         logger.with("signed-string", stringToSign).debug("Computing signature of string");
 
         final byte[] bytes = hmac.doFinal(stringToSign.getBytes());
 
-        return signatureEncoder.encodeToString(bytes);
+        return signatureEncoding.encode(bytes);
     }
 
     public abstract String getStringToSign();
